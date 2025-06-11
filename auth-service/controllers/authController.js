31dc2c3139db9c0421a911108e3968366f8ca5ee
koduri sai vishnu <kodurisaivishnu@gmail.com>
2025-06-11 22:37:53 +0530

    fixed logout cookie expire issue

--- auth-service/controllers/authController.js
+++ auth-service/controllers/authController.js
@@ -98,12 +98,29 @@ export const login = async (req, res) => {
 };
 
 
+// export const logout = (req, res) => {
+//   try {
+//     res.clearCookie("token", {
+//       httpOnly: true,
+//       secure: false, // set to true in production (HTTPS)
+//       sameSite: 'Lax' // or 'None' if cross-site
+//     });
+
+//     res.status(200).json({ message: "Logged out successfully" });
+//   } catch (error) {
+//     console.error("Error in logout controller:", error.message);
+//     res.status(500).json({ message: "Internal server error during logout" });
+//   }
+// };
+
+
 export const logout = (req, res) => {
   try {
-    res.clearCookie("token", {
+    res.clearCookie("jwt", {
       httpOnly: true,
-      secure: false, // set to true in production (HTTPS)
-      sameSite: 'Lax' // or 'None' if cross-site
+      // secure: process.env.NODE_ENV !== "development",
+      secure: true,
+      sameSite: "strict",
     });
 
     res.status(200).json({ message: "Logged out successfully" });
@@ -113,15 +130,6 @@ export const logout = (req, res) => {
   }
 };
 
-
-// export const checkAuth = (req, res) => {
-//   try {
-//     res.status(200).json(req.user);  
-//   } catch (error) {
-//     console.log("Error in checkAuth:", error.message);
-//     res.status(500).json({ message: "Internal server error from checkAuth" });
-//   }
-// };
 
 export const checkAuth = async (req, res) => {
   try {
